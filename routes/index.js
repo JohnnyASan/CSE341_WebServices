c913693a3f4815e4fc804bd48da4caf5ed1b4fd1
JohnnyASan <johnanthonysanabria@gmail.com>
2024-09-16 12:19:15 -0600

    switch Rada to homepage

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
  
-routes.get('/', lesson1Controller.johnnyRoute);
-routes.get('/rada', lesson1Controller.radaRoute);
+routes.get('/', lesson1Controller.radaRoute);
+routes.get('/johnny', lesson1Controller.johnnyRoute);
 routes.get('/gig', lesson1Controller.gigRoute);
 
 module.exports = routes;
