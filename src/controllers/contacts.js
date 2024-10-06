ae7c2e92df8640229bcfcc23405b3b0195f32e2c
Johnny <san23081@byui.edu>
2024-10-05 19:50:50 -0600

    started adding more crud

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -17,7 +17,26 @@ const getById = async (req, res) => {
     res.jsonp(doc);
 };
 
+const postRecord = async (req, res) => {
+    console.log(req);
+    var doc = await mongodb.getDb().db('cse341').collection('contacts').InsertOne({ _id: new ObjectId(),  });
+    res.jsonp(doc._id);
+};
+
+const putRecord = async (req, res) => {
+    var doc = await mongodb.getDb().db('cse341').collection('contacts').updateOne({ _id: new ObjectId(`${req.params.id}`)});
+    res.jsonp(doc);
+}
+
+const deleteRecord = async (req, res) => {
+    var doc = await mongodb.getDb().db('cse341').collection('contacts').deleteOne({ _id: new ObjectId(`${req.params.id}`) });
+    res.jsonp(doc);
+}
+
 module.exports = {
     getAll,
-    getById
+    getById,
+    postRecord,
+    putRecord,
+    deleteRecord
 }
--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -4,5 +4,8 @@ const contactsController = require('../controllers/contacts');
  
 contactsRouter.get('/contacts', contactsController.getAll);
 contactsRouter.get('/contacts/:id', contactsController.getById);
+contactsRouter.post('/contacts', contactsController.postRecord);
+contactsRouter.put('/contacts', contactsController.putRecord);
+contactsRouter.delete('/contacts', contactsController.deleteRecord);
 
 module.exports = contactsRouter;
