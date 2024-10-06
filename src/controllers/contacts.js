62b6938f7fe84c87b36a5df7e47174363a1a15df
JohnnyASan <johnanthonysanabria@gmail.com>
2024-10-05 20:28:52 -0600

    fixed delete

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -60,7 +60,7 @@ const putRecord = async (req, res) => {
 
 const deleteRecord = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDb().db('cse341').collection('contacts').remove({ _id: userId }, true);
+    const response = await mongodb.getDb().db('cse341').collection('contacts').deleteOne({ _id: userId }, true);
     console.log(response);
     if (response.deletedCount > 0) {
         res.status(204).send();
