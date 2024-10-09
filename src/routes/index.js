14c1f9a63e90ced22974f3711c11c67de00552d5
Johnny <san23081@byui.edu>
2024-10-09 17:35:46 -0600

    updated swagger route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../docs/swagger-output.json');
 
 router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', () => {
-    // #swagger.ignore = true
-    swaggerUi.setup(swaggerDocument)
-});
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
  
 router.use('/contacts', require('./contacts'));
